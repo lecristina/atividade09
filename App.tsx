9883ac92ccb957fb049dd44a22e109acf5cea573
lecristina <cristinale33@hotmail.com>
2025-03-26 11:00:15 -0300

    atividade09

--- App.tsx
+++ App.tsx
@@ -1,13 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <View>
+        <Text>Concessionária de veículos</Text>
+        <img src="" alt="" />
+      </View>
+
+
+      <LinearGradient style={{flex:1, width: 300, height:506, borderBottomEndRadius:16, borderBottomLeftRadius:16, padding:24, }} colors={['#ccb1f5','#8d44fc']} start={{x:1,y:0}} end={{x:1,y:0}}>
+ 
+      <View style={styles.estiloInput}>
+        <Text>Fabricante: </Text>
+        <TextInput/>
+        <Text>Modelo: </Text>
+        <TextInput style={styles.estiloInput}/>
+        <Text>Preço: </Text>
+        <TextInput />
+        
+      </View>
+      <Button title='Cadastrar / Salvar'/>
+    </LinearGradient>
+  </View>
+      
+);
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +36,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  estiloInput: {
+    
+  },
+
 });
